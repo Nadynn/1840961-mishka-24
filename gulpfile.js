5628b35b272658461d3b85b9f32b9dfb4ab95aa5
Nadynn <nadynn.balacula@gmail.com>
2021-12-20 00:00:31 +0200

    Стилизация всех страниц3

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,4 @@
-<<<<<<< HEAD
-const gulp = require("gulp");
-const plumber = require("gulp-plumber");
-const sourcemap = require("gulp-sourcemaps");
-const less = require("gulp-less");
-const postcss = require("gulp-postcss");
-const autoprefixer = require("autoprefixer");
-const sync = require("browser-sync").create();
 
-// Styles
-
-const styles = () => {
-  return gulp.src("source/less/style.less")
-    .pipe(plumber())
-    .pipe(sourcemap.init())
-=======
 import gulp from 'gulp';
 import plumber from 'gulp-plumber';
 import less from 'gulp-less';
@@ -26,12 +11,12 @@ import browser from 'browser-sync';
 export const styles = () => {
   return gulp.src('source/less/style.less', { sourcemaps: true })
     .pipe(plumber())
->>>>>>> 9f6a15b1f8608a660cdfc0ec113e9e5158c9ed46
+
     .pipe(less())
     .pipe(postcss([
       autoprefixer()
     ]))
-<<<<<<< HEAD
+
     .pipe(sourcemap.write("."))
     .pipe(gulp.dest("source/css"))
     .pipe(sync.stream());
@@ -43,16 +28,7 @@ exports.styles = styles;
 
 const server = (done) => {
   sync.init({
-=======
-    .pipe(gulp.dest('source/css', { sourcemaps: '.' }))
-    .pipe(browser.stream());
-}
 
-// Server
-
-const server = (done) => {
-  browser.init({
->>>>>>> 9f6a15b1f8608a660cdfc0ec113e9e5158c9ed46
     server: {
       baseDir: 'source'
     },
@@ -63,7 +39,6 @@ const server = (done) => {
   done();
 }
 
-<<<<<<< HEAD
 exports.server = server;
 
 // Watcher
@@ -74,16 +49,6 @@ const watcher = () => {
 }
 
 exports.default = gulp.series(
-=======
-// Watcher
 
-const watcher = () => {
-  gulp.watch('source/less/**/*.less', gulp.series(styles));
-  gulp.watch('source/*.html').on('change', browser.reload);
-}
-
-
-export default gulp.series(
->>>>>>> 9f6a15b1f8608a660cdfc0ec113e9e5158c9ed46
   styles, server, watcher
 );

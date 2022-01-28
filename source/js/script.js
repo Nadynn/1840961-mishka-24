df8d37c18c55c347dccd652a3151b18b6ac933ed
Nadynn <nadynn.balacula@gmail.com>
2022-01-28 13:51:26 +0200

    Автоматизация сборки личного проекта test

---
+++ source/js/script.js
@@ -0,0 +1,14 @@
+var navMenu= document.querySelector('.menu-nav');
+var navToggle = document.querySelector('.menu-nav__toggle');
+
+navMenu.classList.remove('menu-nav--nojs');
+
+navToggle.addEventListener('click', function() {
+if (navMenu.classList.contains('menu-nav--closed')) {
+      navMenu.classList.remove('menu-nav--closed');
+      navMenu.classList.add('menu-nav--opened');
+    } else {
+      navMenu.classList.add('menu-nav--closed');
+      navMenu.classList.remove('menu-nav--opened');
+    }
+});

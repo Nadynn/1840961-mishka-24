ee42033459265e3ac0114919457bd1b8715a1340
Nadynn <nadynn.balacula@gmail.com>
2022-02-05 17:50:52 +0200

    Улучшение скорости загрузки

--- gulpfile.js
+++ gulpfile.js
@@ -75,7 +75,7 @@ const svg = () =>
     .pipe(gulp.dest('build/img'));
 
 const sprite = () => {
-  return gulp.src('source/img/*.svg')
+  return gulp.src('source/img/**/*.svg')
     .pipe(svgo())
     .pipe(svgstore({
       inlineSvg: true
